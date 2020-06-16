0d85aae75166fba9302c796b0cc0d68575960d38
abdullah <Ab.alsayegh2004@gmail.com>
2020-06-16 16:06:44 +0300

    الواجب ١

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -7,10 +7,10 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+ let lastname = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -19,7 +19,7 @@ var weightSum = myWeight + FriendWeight
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = myWeight / (height * height)
 
 /*
 - - - -
@@ -38,35 +38,41 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "abdullah"
+var lastName1 = "alsayegh"
+var email1 = "Ab.alsayegh2004@gmail.com"
+var phoneNumber1 = "98590456"
+var age1 = 15
+var country1 = "kuwait"
+var area1 = "mubark alabdullah"
+var password = "12345678"
+var isKuwaiti = true
 
 //Person 2
 
-
-
-
-
-
-
+var firstName2 = "aziz"
+var lastName2 = "alsaleh"
+var email2 = "aziz2004@gmail.com"
+var phoneNumber2 = "44879358"
+var age2 = 15
+var country2 = "UAE"
+var area2 = "sharja"
+var password2 = "46347458"
+var isKuwaiti2 = true
 
 
 
 //Person 3
 
-
-
-
-
-
-
+var firstName3 = "ali"
+var lastName3 = "alkhaldy"
+var email3 = "jaber2004@gmail.com"
+var phoneNumber3 = "38592760"
+var age3 = 18
+var country3 = "kuwait"
+var area3 = "mubark alabdullah"
+var password3 = "29778216"
+var isKuwaiti3 = false
 
 
 /*
@@ -82,3 +88,29 @@ var isKuwaiti = false
 
  
  */
+
+age1 != age2
+age2 != age3
+age3 != age1
+
+area1 != area2
+area2 != area3
+area3 != area1
+
+age1 > 18
+age2 > 18
+age3 > 18
+
+age1 < 18
+age2 < 18
+age3 < 18
+
+
+age1 == 17
+age2 == 17
+age3 == 17
+
+
+age1 != 17
+age2 != 17
+age3 != 17
